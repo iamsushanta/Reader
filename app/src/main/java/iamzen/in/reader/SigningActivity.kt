bdd490cd6127d97897ce9bcd2d5783b1c532782f
Sushanta Das <iamsushanta@protonmail.com>
2022-02-02 18:12:00 +0530

    add file

---
+++ app/src/main/java/iamzen/in/reader/SigningActivity.kt
@@ -0,0 +1,11 @@
+package iamzen.`in`.reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signinig)
+    }
+}
